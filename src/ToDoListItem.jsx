6c6f0f39d974187c8d000de80a004c621e43bd4e
Ava kazeminejad <avaknejad@gmail.com>
2022-08-27 10:11:49 +0430

    figma implements

---
+++ src/ToDoListItem.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const ToDoListItem = props =>{
+    return<li><label><input type="checkbox"/></label></li>;
+
+};
+
+export const ToDoList = props =>{
+    return<li><label><input type="edit"/></label></li>
+}
